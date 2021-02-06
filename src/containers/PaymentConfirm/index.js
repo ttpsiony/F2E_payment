dcd5cb2e4d3b0b39e82c2b2d1cc377d06d72e088
William_WC_LIN <William_WC_Lin@wiedu.com>
2021-02-06 15:52:33 +0800

    chore: deploy to github

--- src/containers/PaymentConfirm/index.js
+++ src/containers/PaymentConfirm/index.js
@@ -20,13 +20,13 @@ export default function Confirm() {
 		}
 		const isToRedirect = Object.keys(receiver).some((el) => el === '');
 		if (isToRedirect || !paymentApproach) {
-			history.push('/step1');
+			history.push('/payment/step1');
 		}
 	}, [paymentApproach, receiver, history]);
 
 	const onClickInitPaymentHandler = () => {
 		dispatch(actions.initPaymentInfo());
-		history.push('/step1');
+		history.push('/payment/step1');
 	};
 
 	return (
